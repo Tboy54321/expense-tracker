5028bbf915c480c5e6a8e6c390ac91b55e9ded2e
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-12-12 23:42:58 +0100

    updated

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import MySQLdb.cursors, re, hashlib
+#from flask_mysqldb import MySQL
+#import MySQLdb.cursors
+#import MySQLdb.cursors, re, hashlib
+import mysql.connector
 
+connection = mysql.connector.connect(host='localhost',
+        database='pythonlogin', user='root', password='')
+
+cursor = connection.cursor()
 app = Flask(__name__)
+
 app.secret_key = 'oluwatimothyllll2222'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'oluwatimothyllll2222'
-app.config['MYSQL_DB'] = 'pythonlogin'
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = ''
+#app.config['MYSQL_DB'] = 'pythonlogin'
 
-mysql = MySQL(app)
+#mysql = MySQL(app)
 
 
 @app.route('/')
@@ -22,29 +28,33 @@ def home():
 def login():
     msg=''
     # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST':
         # Create variables for easy access
-        username = request.form['username']
+        #username = request.form['username']
         password = request.form['password']
+        email = request.form['email']
         # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        ##cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        #cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM accounts WHERE email = %s AND password = %s', (email, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
+        print(account)
         # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            session['email'] = account['email']
-            return render_template('index.html')
+            session['id'] = account[0]
+            #session['username'] = account['username']
+            session['email'] = account[3]
+            return redirect(url_for('home'))
             # Redirect to home page
-            return 'Logged in successfully!'
+            #return 'Logged in successfully!'
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
     return render_template('login.html', msg = msg)
+    #return '<h1>WRONG PASSWORD</h1>'
 
 
 @app.route('/settings')
